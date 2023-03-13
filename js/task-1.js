219619e4469bbf905f870d6551c5df1f9de7f502
Ivan Hubarenko <103453171+Hubarenko10@users.noreply.github.com>
2023-03-13 22:19:46 +0200

    ed

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,32 @@
-const btn = document.querySelector('.btn')
-console.log(btn)
+const btn = document.getElementById("convertBtn");
+const resultElement = document.getElementById("result");
+const fromInput = document.getElementById("fromUnit");
+const toInput = document.getElementById("toUnit");
+const distanceInput = document.getElementById("fromValue");
+
+fetch("../data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+
+    btn.addEventListener("click", () => {
+      convert(data);
+    });
+  })
+  .catch((error) => console.error(error));
+
+function convert(data) {
+  const distance = parseFloat(distanceInput.value);
+  if (isNaN(distance)) {
+    alert("Please enter a valid distance.");
+    return;
+  }
+  const from = fromInput.value;
+  const to = toInput.value;
+  if (!data[from] || !data[to]) {
+    alert("Please select valid units.");
+    return; 
+  }
+  let result = distance * data[from][to];
+  resultElement.innerText = `${distance} ${from} is equal to ${result.toFixed(2)} ${to}.`;
+}
